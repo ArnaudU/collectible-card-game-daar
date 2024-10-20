7a842a4efc8c5f861d51feda93619e588a1ca270
Jm <jean-marc.zhuang.1@etu.sorbonne-universite.fr>
2024-10-20 15:39:20 +0200

    contrat fait

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -5,56 +5,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Collection is ERC721, Ownable {
-  struct Card {
-    string name;
-    string imgURL;
-  }
+    struct Card {
+        string name;
+        string imgURL;
+    }
 
-  event NewCard(string name, string imgURL);
+    event NewCard(string name, string imgURL);
+    event MintCard(address indexed to, uint256 indexed cardNumber);
 
-  mapping(uint256 => address) public cardToOwner;
-  mapping(address => uint256) public ownerCardsCount;
+    string public collectionName;
+    Card[] public cards;
 
-  string public collectionName;
-  address public admin;
+    constructor(string memory _name) ERC721(_name, "PKMN") Ownable(msg.sender) {
+        collectionName = _name;
+    }
 
-  Card[] public cards;
+    function addCard(string memory _name, string memory _imgURL) external onlyOwner {
+        cards.push(Card(_name, _imgURL));
+        emit NewCard(_name, _imgURL);
+    }
 
-  constructor(string memory _name) ERC721(_name, "PKMN") Ownable(msg.sender) {
-    collectionName = _name;
-    admin = msg.sender;
-  }
+    function mintCard(address _to, uint256 _cardNumber) external onlyOwner {
+        require(_cardNumber < cards.length, "Invalid card number");
+        _safeMint(_to, _cardNumber);
+        emit MintCard(_to, _cardNumber);
+    }
 
-  function addCard(string memory _name, string memory _imgURL) external {
-    require(msg.sender == admin);
-    cards.push(Card(_name, _imgURL));
-    emit NewCard(_name, _imgURL);
-  }
-
-  function mintCard(address _to, uint256 _cardNumber) external {
-    require(msg.sender == admin);
-    cardToOwner[_cardNumber] = _to;
-    ownerCardsCount[_to]++;
-    _safeMint(_to, _cardNumber);
-  }
-
-  function transferCard(address _from, address _to, uint256 _tokenId) external {
-    require(_from == ownerOf(_tokenId));
-    cardToOwner[_tokenId] = _to;
-    ownerCardsCount[_from]--;
-    ownerCardsCount[_to]++;
-    safeTransferFrom(_from, _to, _tokenId);
-  }
-
-  function balanceOf(
-    address _owner
-  ) public view override returns (uint256 _balance) {
-    return ownerCardsCount[_owner];
-  }
-
-  function ownerOf(
-    uint256 _tokenId
-  ) public view override returns (address _owner) {
-    return cardToOwner[_tokenId];
-  }
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_tokenId < cards.length, "Nonexistent token");
+        return cards[_tokenId].imgURL;
+    }
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -1,29 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.0;
 
 import "./Collection.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Main {
-  mapping(string => Collection) private collections;
+contract Main is Ownable {
+    event CollectionCreated(string collectionName, address collectionAddress);
 
-  function createCollection(string calldata name) external {
-    collections[name] = new Collection(name);
-  }
+    mapping(string => Collection) private collections;
 
-  function _addCard(
-    string memory _setName,
-    string memory _cardName,
-    string memory _imgURL
-  ) public {
-    collections[_setName].addCard(_cardName, _imgURL);
-  }
+    constructor() Ownable(msg.sender) {}
 
-  function mintCardToUser(
-    string memory collectionName,
-    address recipient,
-    uint256 cardId
-  ) public {
-    collections[collectionName].mintCard(recipient, cardId);
-  }
+    function createCollection(string calldata _name) external onlyOwner {
+        Collection newCollection = new Collection(_name);
+        collections[_name] = newCollection;
+        emit CollectionCreated(_name, address(newCollection));
+    }
+
+    function addCard(string memory _setName, string memory _cardName, string memory _imgURL) public onlyOwner {
+        require(address(collections[_setName]) != address(0), "Collection does not exist");
+        collections[_setName].addCard(_cardName, _imgURL);
+    }
+
+    function mintCardToUser(string memory _collectionName, address recipient, uint256 cardId) public onlyOwner {
+        require(address(collections[_collectionName]) != address(0), "Collection does not exist");
+        collections[_collectionName].mintCard(recipient, cardId);
+    }
+
+    function mintMultipleCardsToUser(string memory _collectionName, address _to, uint256[] memory cardIds) public onlyOwner {
+        require(address(collections[_collectionName]) != address(0), "Collection does not exist");
+        for (uint256 i = 0; i < cardIds.length; i++) {
+            collections[_collectionName].mintCard(_to, cardIds[i]);
+        }
+    }
 }
